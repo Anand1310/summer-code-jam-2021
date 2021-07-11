021b70d9e78598c6e16560fa5c57abf0915a44a7
Jason Ho <50993239+Jason11ookJJ@users.noreply.github.com>
2021-07-11 21:08:59 +0800

    reformat2

--- core/maze.py
+++ core/maze.py
@@ -1,16 +1,14 @@
 import random
-
 # Easy to read representation for each cardinal direction.
+from typing import List, Literal
+
 N, S, W, E = ("n", "s", "w", "e")
 
 
 class Cell(object):
-    """
-    Class for each individual cell. Knows only its position and which walls are
-    still standing.
-    """
+    """Class for each individual cell. Knows only its position and which walls are still standing."""
 
-    def __init__(self, x, y, walls):
+    def __init__(self, x: int, y: int, walls: Literal[N, S, W, E]):
         self.x = x
         self.y = y
         self.walls = set(walls)
@@ -19,24 +17,20 @@ class Cell(object):
         # <15, 25 (es  )>
         return "<{}, {} ({:4})>".format(self.x, self.y, "".join(sorted(self.walls)))
 
-    def __contains__(self, item):
+    def __contains__(self, item: object) -> bool:
         # N in cell
         return item in self.walls
 
-    def is_full(self):
-        """
-        Returns True if all walls are still standing.
-        """
+    def is_full(self) -> bool:
+        """Returns True if all walls are still standing."""
         return len(self.walls) == 4
 
-    def _wall_to(self, other):
+    def _wall_to(self, other: object) -> Literal[N, S, W, E]:
         """
         Returns the direction to the given cell from the current one.
+
         Must be one cell away only.
         """
-        assert abs(self.x - other.x) + abs(self.y - other.y) == 1, "{}, {}".format(
-            self, other
-        )
         if other.y < self.y:
             return N
         elif other.y > self.y:
@@ -45,21 +39,15 @@ class Cell(object):
             return W
         elif other.x > self.x:
             return E
-        else:
-            assert False
 
-    def connect(self, other):
-        """
-        Removes the wall between two adjacent cells.
-        """
+    def connect(self, other: List[object]) -> None:
+        """Removes the wall between two adjacent cells."""
         other.walls.remove(other._wall_to(self))
         self.walls.remove(self._wall_to(other))
 
 
 class Maze(object):
-    """
-    Maze class containing full board and maze generation algorithms.
-    """
+    """Maze class containing full board and maze generation algorithms."""
 
     # Unicode character for a wall with other walls in the given directions.
     UNICODE_BY_CONNECTIONS = {
@@ -80,10 +68,8 @@ class Maze(object):
         "w": "╴",
     }
 
-    def __init__(self, width=20, height=10):
-        """
-        Creates a new maze with the given sizes, with all walls standing.
-        """
+    def __init__(self, width: int = 20, height: int = 10):
+        """Creates a new maze with the given sizes, with all walls standing."""
         self.width = width
         self.height = height
         self.cells = []
@@ -91,20 +77,19 @@ class Maze(object):
             for x in range(self.width):
                 self.cells.append(Cell(x, y, [N, S, E, W]))
 
-    def __getitem__(self, index):
-        """
-        Returns the cell at index = (x, y).
-        """
+    def __getitem__(self, index: set):
+        """Returns the cell at index = (x, y)."""
         x, y = index
         if 0 <= x < self.width and 0 <= y < self.height:
             return self.cells[x + y * self.width]
         else:
             return None
 
-    def neighbors(self, cell):
+    def neighbors(self, cell: Cell) -> list:
         """
-        Returns the list of neighboring cells, not counting diagonals. Cells on
-        borders or corners may have less than 4 neighbors.
+        Returns the list of neighboring cells, not counting diagonals.
+
+        Cells on borders or corners may have less than 4 neighbors.
         """
         x = cell.x
         y = cell.y
@@ -113,11 +98,8 @@ class Maze(object):
             if neighbor is not None:
                 yield neighbor
 
-    def to_list(self):
-        """
-        Returns a matrix with a pretty printed visual representation of this
-        maze.
-        """
+    def to_list_matrix(self) -> list:
+        """Returns a matrix with a pretty printed visual representation of this maze."""
         str_matrix = [["W"] * (self.width * 2 + 1) for i in range(self.height * 2 + 1)]
 
         for cell in self.cells:
@@ -137,8 +119,9 @@ class Maze(object):
 
     def __repr__(self):
         """
-        Returns an Unicode representation of the maze. Size is doubled
-        horizontally to avoid a stretched look. Example 5x5:
+        Returns an Unicode representation of the maze. Size is doubled horizontally to avoid a stretched look.
+
+        Example 5x5:
         ┌───┬───────┬───────┐
         │   │       │       │
         │   │   ╷   ╵   ╷   │
@@ -154,7 +137,7 @@ class Maze(object):
         # Starts with regular representation. Looks stretched because chars are
         # twice as high as they are wide (look at docs example in
         # `Maze._to_str_matrix`).
-        skinny_matrix = self.to_list()
+        skinny_matrix = self.to_list_matrix()
 
         # Simply duplicate each character in each line.
         double_wide_matrix = []
@@ -168,12 +151,8 @@ class Maze(object):
         # So we remove the last char of each line.
         matrix = [line[:-1] for line in double_wide_matrix]
 
-        def g(x, y):
-            """
-            Returns True if there is a wall at (x, y). Values outside the valid
-            range always return false.
-            This is a temporary helper function.
-            """
+        def g(x: int, y: int) -> bool:
+            """Returns True if there is a wall at (x, y). Values outside the valid range always return false."""
             if 0 <= x < len(matrix[0]) and 0 <= y < len(matrix):
                 return matrix[y][x] != " "
             else:
@@ -214,9 +193,10 @@ class Maze(object):
         # Simple double join to transform list of lists into string.
         return "\n".join("".join(line) for line in matrix) + "\n"
 
-    def randomize(self):
+    def randomize(self) -> None:
         """
         Knocks down random walls to build a random perfect maze.
+
         Algorithm from http://mazeworks.com/mazegen/mazetut/index.htm
         """
         cell_stack = []
@@ -235,10 +215,8 @@ class Maze(object):
                 cell = cell_stack.pop()
 
     @classmethod
-    def generate(cls, width=20, height=10):
-        """
-        Returns a new random perfect maze with the given sizes.
-        """
+    def generate(cls, width: int = 20, height: int = 10):
+        """Returns a new random perfect maze with the given sizes."""
         m = cls(width, height)
         m.randomize()
         return m
@@ -258,7 +236,7 @@ if __name__ == "__main__":
         height = 10
 
     maze = Maze.generate(width, height)
-    q = maze.to_list()
+    q = maze.to_list_matrix()
     for i in q:
         print(i)
     print(maze)
