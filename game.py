f98f10dc2f85c08411fc64edbcaeab4ff3423d19
StoneSteel27 <stonesteel27@gmail.com>
2021-07-11 18:28:21 +0530

    created camera and camera transition

--- game.py
+++ game.py
@@ -2,6 +2,9 @@
 import logging
 import os
 from typing import Iterable, Iterator, Union
+import pytweening as pt
+import numpy as np
+import time
 
 import blessed
 from blessed.keyboard import Keystroke
@@ -9,7 +12,6 @@ from blessed.keyboard import Keystroke
 if "logs" not in os.listdir():
     os.mkdir("logs")
 logging.basicConfig(filename="logs/debug.log", level=logging.DEBUG)
-
 
 NEXT_SCENE = 1
 RESET = 2
@@ -67,3 +69,41 @@ class Game:
                     continue
 
                 val = term.inkey(timeout=3)
+
+
+class Camera:
+    def __init__(self, game_map: np.ndarray, cam_x: int = 0, cam_y: int = 0, quickness: float = 0.0) -> None:
+        """
+        :param cam_x: initial position of camera's x coordinates
+        :param cam_y: initial position of camera's y coordinates
+        :param game_map: The whole game map
+        :param quickness: how fast camera should transition from one point to another
+        """
+        self.game_map = game_map
+        self._cam_x = cam_x
+        self._cam_y = cam_y
+        self.quickness = quickness
+
+    @property
+    def cam_x(self):
+        return self._cam_x
+
+    @property
+    def cam_y(self) -> int:
+        return self._cam_y
+
+    @cam_x.setter
+    def cam_x(self, value) -> None:
+        pass
+
+    def _set_pos(self, x, y) -> None:
+        interval = 1 * (1 - self.quickness)
+        transtion_lenght = int(1 / interval)
+        for i in range(transtion_lenght):
+            time.sleep(interval)
+            self._cam_x *= pt.easeInOutSine(interval * i)
+            self._cam_y *= pt.easeInOutSine(interval * i)
+
+    def _render(self, x, y):
+        # Todo: create the render func and handle errors
+        pass
