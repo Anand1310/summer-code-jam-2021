2f6e4eabc75849a272af15fe87e1659c61065407
Pritam Dey <pdeypritam@gmail.com>
2021-07-14 23:56:57 +0530

    fix openal bug

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from typing import List
+from openal import oalQuit
 
 from game import Game, Scene
 from levels import EndScene, Level, TitleScene
@@ -7,9 +8,10 @@ from levels import EndScene, Level, TitleScene
 if __name__ == "__main__":
     scenes: List[Scene] = [TitleScene()]
     if len(sys.argv) == 1:
-        scenes.extend([Level(str(i)) for i in range(1, 5)])
+        scenes.extend([Level(str(i)) for i in range(1, 9)])
     else:
         scenes.append(Level(sys.argv[1]))
     scenes.append(EndScene())
     game = Game(scenes)
     game.run()
+    oalQuit()
