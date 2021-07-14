50415a87b5d4bdddde1899396439f0fdac3f17cb
Pritam Dey <pdeypritam@gmail.com>
2021-07-14 14:57:51 +0530

    add player class and sounds

---
+++ core/sound.py
@@ -0,0 +1,19 @@
+from openal import oalOpen
+
+from utils import Vec  # type: ignore
+
+enter_box_sound = oalOpen("sound/enter_box.wav")
+hit_wall_sound = oalOpen("sound/hit_wall.wav")
+
+
+def play_enter_box_sound() -> None:
+    """Sound effect for entering a box"""
+    # https://mixkit.co/free-sound-effects/
+    enter_box_sound.play()
+
+
+def play_hit_wall_sound(direction: Vec) -> None:
+    """Sound effect for hitting a wall"""
+    # https://mixkit.co/free-sound-effects/
+    hit_wall_sound.set_position((direction.x, 0, direction.y))
+    hit_wall_sound.play()
--- levels.py
+++ levels.py
@@ -1,17 +1,15 @@
 """Examples for designing levels."""
 import time
-from copy import copy
 from threading import Thread
 from typing import Union
 
 import blessed
 from blessed.keyboard import Keystroke
 
-from core.cursor import Cursor
+from core.cursor import Player
 from core.maze import Maze
 from core.render import Render
 from game import NEXT_SCENE, QUIT, RESET, Scene
-from utils import Vec  # type: ignore
 
 term = blessed.Terminal()
 render = Render()
@@ -52,50 +50,16 @@ class TitleScene(Scene):
         pass
 
 
-class Level_1(Scene):
-    """Example of a level."""
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.first_line = term.clear + "hit 'n' to end the game\n" + term.home
-        self.first_frame = True
-
-    def next_frame(self, val: Keystroke) -> Union[str, int]:
-        """Return next frame to render"""
-        if self.first_frame:
-            self.first_frame = False
-            print(self.first_line)
-        if not val:
-            new_line = ""
-        elif val.is_sequence:
-            new_line = "got sequence: {0}.".format((str(val), val.name, val.code))
-        elif val.lower() == "n":
-            return NEXT_SCENE
-        elif val.lower() == "q":
-            return QUIT
-        elif val.lower() == "r":
-            return RESET
-        elif val:
-            new_line = "got {0}.".format(val)
-
-        return new_line
-
-    def reset(self) -> None:
-        """Reset the current level"""
-        self.first_frame = True
-
-
-class Level_2(Scene):
+class Level(Scene):
     """First basic game"""
 
     def __init__(self, level: str = "1") -> None:
         super().__init__()
         self.maze = Maze.load(level)
 
-        self.avi = Cursor(
-            self.mat2screen(mat=self.maze.start), fill="█", speed=Vec(1, 1)
-        )
-        self.end_loc = self.mat2screen(self.maze.end)
+        self.player = Player(self.maze.mat2screen(mat=self.maze.start))
+
+        self.end_loc = self.maze.mat2screen(self.maze.end)
         self.first_frame = True
         self.maze_is_visible = False
         for box in self.maze.boxes:
@@ -114,23 +78,21 @@ class Level_2(Scene):
             frame += term.move_xy(*self.end_loc) + "&"  # type: ignore
             render(frame)
             for box in self.maze.boxes:
-                box.render(self.avi.coords)
-            self.avi.render()
+                box.render(self.player)
+
+            self.player.start()
 
         elif val.is_sequence and (257 < val.code < 262):
-            avi_loc = self.avi.loc_on_move(val.name)
-
-            if self.wall_at(avi_loc):
-                # collision count and music code goes here
-                pass
-                # logging.info(f"hit maze @ {avi_loc}")
-            elif all(self.avi.coords == self.end_loc):
+            # update player
+            self.player.update(val, self.maze)
+            # check if game ends
+            if all(self.player.avi.coords == self.end_loc):
                 return NEXT_SCENE
-            else:
-                self.avi.move(val.name)
+            # render boxes and mazes
             for box in self.maze.boxes:
-                box.render(self.avi.coords)
-            self.avi.render()
+                box.render(self.player)
+            # render player
+            self.player.render()
 
         elif val.lower() == "q":
             return QUIT
@@ -139,69 +101,27 @@ class Level_2(Scene):
         elif val.lower() == "h":
             if not self.maze_is_visible:
                 self.maze_is_visible = True
-                # frame = self.draw_maze(self.maze.draw(), update_cursor=False)  # type: ignore
-                # frame += self.avi.render()  # type: ignore
-                self.draw_maze(self.maze.map, update_cursor=False)  # type: ignore
-                self.avi.render()  # type: ignore
-                # return frame
+                render(self.maze.map)
+                self.player.render()
             else:
                 self.maze_is_visible = False
                 self.remove_maze(0)
                 return ""
         return ""
 
-    def draw_maze(self, maze: str, update_cursor: bool = True) -> None:
-        """Draw main maze"""
-        if update_cursor:
-            self.avi.coords = self.maze.top_left_corner + self.maze.start * (2, 1)
-        frame = term.move_xy(*self.maze.top_left_corner) + maze
-        render(frame)
-        # return
-
     def remove_maze(self, sleep: float = 2) -> None:
         """Erase main maze"""
         time.sleep(sleep)
-        maze = self.maze.map
-        for chr in "┼├┴┬┌└─╶┤│┘┐╷╵╴":
-            if chr in maze:
-                maze = maze.replace(chr, " ")
-        self.draw_maze(maze, update_cursor=False)  # erase maze
-        # frame = self.draw_maze(maze, update_cursor=False)  # erase maze
-        # frame += self.avi.render()  # draw avi
-        self.avi.render()  # draw avi
+        render(self.maze.erase_map)
+        self.player.render()
         for box in self.maze.boxes:
-            box.render(self.avi.coords)
-
-    def wall_at(self, screen: Vec) -> bool:
-        """Return True if there is a wall at (x, y). Values outside the valid range always return False."""
-        x, y = self.screen2mat(screen)
-        screen = screen - self.maze.top_left_corner
-        # logging.info(
-        #     f"{x=}\t{screen.x=}\t{self.maze.matrix[y][x]}, {self.maze.matrix[y][x-1]}, {self.maze.matrix[y][x+1]}"
-        # )
-        if 0 <= x < len(self.maze.matrix[0]) and 0 <= y < len(self.maze.matrix):
-            is_wall = self.maze.matrix[y][x] == 1
-            # if is_wall and screen.x == 2 * x + 1:
-            #     return False
-            # else:
-            #     return True
-            return is_wall
-        else:
-            return False
-
-    def screen2mat(self, screen: Vec) -> Vec:
-        """Convert screen location of a point to matrix location"""
-        return (screen - self.maze.top_left_corner) // (2, 1)
-
-    def mat2screen(self, mat: Vec) -> Vec:
-        """Convert matrix location of a point to screen location"""
-        return self.maze.top_left_corner + mat * (2, 1)
+            box.render(self.player)
 
     def reset(self) -> None:
         """Reset this level"""
         for box in self.maze.boxes:
             box.needs_cleaning = False
-        self.avi.coords = copy(self.maze.start)
+        self.player.start()
         self.first_frame = True
 
 
