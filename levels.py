c73324c24d9acab2ca605fd41dfc10207ba8083b
Pritam Dey <pdeypritam@gmail.com>
2021-07-17 18:55:20 +0530

    fix linting

--- levels.py
+++ levels.py
@@ -14,7 +14,9 @@ from core.maze import AIR, Box, Maze
 from core.player import MenuCursor, Player
 from core.render import Render
 from core.sound import enter_game_sound, play_level_up_sound, stop_bgm
-from game import CREDITS, LOSE, NEXT_SCENE, PAUSE, PLAY, QUIT, RESET, TITLE, Scene
+from game import (
+    CREDITS, LOSE, NEXT_SCENE, PAUSE, PLAY, QUIT, RESET, TITLE, Scene
+)
 from utils import Boundary, Vec  # type: ignore
 
 term = blessed.Terminal()
