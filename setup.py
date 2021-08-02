b86f8af725a237973202138f4683fd3bad1bebc9
Jason Ho <50993239+Jason11ookJJ@users.noreply.github.com>
2021-08-02 22:13:49 +0800

    update version number to 1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="maze_gitb",  # temp name
-    version="0.1a9",
+    version="1.0",
     license="MIT",
     description="A maze game that required users to think inside the box to win",
     long_description=long_description,
@@ -18,7 +18,7 @@ setup(
     project_urls={
         "Bug Tracker": "https://github.com/Anand1310/summer-code-jam-2021/issues",
     },
-    download_url="https://github.com/Anand1310/summer-code-jam-2021/archive/v_01.tar.gz",
+    download_url="https://github.com/Anand1310/summer-code-jam-2021/archive/v1_0.tar.gz",
     keywords=["game", "maze", "box", "think inside the box", "sound", "3d-sound"],
     install_requires=[
         'blessed',
@@ -33,7 +33,7 @@ setup(
         "Natural Language :: English",
         "Operating System :: Microsoft :: Windows",
         "Operating System :: Unix",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3",
         "Topic :: Games/Entertainment :: Puzzle Games",
     ],
     entry_points={
@@ -44,5 +44,5 @@ setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     include_package_data=True,
-    python_requires=">=3",
+    python_requires=">=3.8.6",
 )
